db8048c501858e21d199521631433c8a1a67ea90
Shakib <132547096+mr-shakib@users.noreply.github.com>
2025-05-13 21:53:44 +0600

    Add files via upload

---
+++ 1022A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        cout << ((n * n) / 4) + 1 << endl;
+    }
+
+    return 0;
+}
